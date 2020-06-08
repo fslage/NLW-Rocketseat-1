a783cc434a9b75d614e1f3c21b36d5b99d9856aa
Flage <fslage7@gmail.com>
2020-06-08 13:48:38 -0300

    NLW

--- public/js/create-point.js
+++ public/js/create-point.js
@@ -1,14 +1,14 @@
 function populateUFs() {
     const ufSelect = document.querySelector("select[name=uf]");
-
+    
     fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
-        .then(res => res.json())
-        .then(states => {
+    .then( res => res.json())
+    .then( states => {
 
-            for (const state of states) {
-                ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`;
-            }
-        });
+        for(const state of states) {
+            ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`;
+        }
+    });
 }
 
 populateUFs();
@@ -16,31 +16,31 @@ populateUFs();
 function getCities(event) {
     const citySelect = document.querySelector("select[name=city]");
     const stateInput = document.querySelector("input[name=state]");
-
+    
     const ufValue = event.target.value;
-
+    
     const indexOfSelectecState = event.target.selectedIndex;
     stateInput.value = event.target.options[indexOfSelectecState].text;
-
+    
     const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`;
 
     citySelect.innerHTML = "<option value>Selecione a cidade</option>";
     citySelect.disabled = true;
 
     fetch(url)
-        .then(res => res.json())
-        .then(cities => {
-
-            for (const city of cities) {
-                citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
+            .then( res => res.json())
+            .then( cities => {
+                
+                for(const city of cities) {
+                    citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
             }
 
             citySelect.disabled = false;
-        });
+            });
 }
 
 document.querySelector("select[name=uf]")
-    .addEventListener("change", getCities);
+        .addEventListener("change", getCities);
 
 // Itens de coleta
 
@@ -56,7 +56,7 @@ const collectedItems = document.querySelector("input[name=items");
 let selectedItems = [];
 
 function handleSelectedItem(event) {
-
+    
     const itemLi = event.target;
 
     // adicionar ou remover uma classe com JS
@@ -69,23 +69,23 @@ function handleSelectedItem(event) {
     const alreadySelected = selectedItems.findIndex(item => {
         const itemFound = item == itemId; // será true ou false
         return itemFound;
-
     });
 
     // se já estiver selecionado tirar da seleção
-    if (alreadySelected >= 0) {
+    if(alreadySelected >= 0) {
         const filteredItems = selectedItems.filter(item => {
             const itemIsDifferent = item != itemId;
-            return itemIsDifferent;
+            return itemIsDifferent;        
         });
 
         selectedItems = filteredItems;
-    } else {
-        // se não estiver selecionado, adicionar à seleção
-        selectedItems.push(itemId);
-    }
-
+    } else {    
+    // se não estiver selecionado, adicionar à seleção
+    selectedItems.push(itemId);
+    }   
+    
     // atualizar o campo escondido com os itens selecionados
     collectedItems.value = selectedItems;
 
 }
+
